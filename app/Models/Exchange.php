1ac274a4cdedd3f6abaafea2c91467cee9165765
AdonayHernandez <139892497+AdonayHernandez@users.noreply.github.com>
2025-04-04 14:24:20 -0600

    Intercambio Realizado

--- app/Models/Exchange.php
+++ app/Models/Exchange.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Exchange extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\ExchangeFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
 }
